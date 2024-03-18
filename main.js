573fab50c77221b9975b27026124a87ffa504b30
azmiindri13 <azmii.indri03@gmail.com>
2024-03-18 15:43:36 +0700

    first

---
+++ main.js
@@ -0,0 +1,9 @@
+const barMenu=document.querySelector('.bar-menu');
+
+function openMenu(){
+    document.querySelector('.menu').style.right='0'
+}
+
+function closeMenu(){
+    document.querySelector('.menu').style.right='-300px'
+}
